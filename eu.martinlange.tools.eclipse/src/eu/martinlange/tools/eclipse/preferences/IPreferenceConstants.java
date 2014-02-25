2fa252b1592e6f8ed0d900631977619ec6c328d5
Martin <mail@martinlange.eu>
2014-02-25 18:12:31 +0100

    version# updated

--- eu.martinlange.tools.eclipse/src/eu/martinlange/tools/eclipse/preferences/IPreferenceConstants.java
+++
@@ -1,9 +0,0 @@
-package eu.martinlange.tools.eclipse.preferences;
-
-public interface IPreferenceConstants {
-
-	public static final String STYLES = "styles";
-
-	public static final String PATTERN = "pattern";
-
-}
